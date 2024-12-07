4b46946574b8eac4b9ae2d14ffd51690e9ad51b1
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2024-12-07 09:48:33 +0100

    changes to other files

--- components/modals/AddExpensesModal.js
+++ components/modals/AddExpensesModal.js
@@ -1,7 +1,16 @@
 import Modal from '@/components/Modal'
+import { useState } from 'react'
 function AddExpensesModal({show,onClose}){
+
+    const [expenseAmount,setExpenseAmount] = useState("")
+
     return(<Modal show={show} onClose={onClose}>
-        <h1>Hello I'm an enjc</h1>
+        <div className='flex flex-col gap-4'>
+            <label>Enter an amount</label>
+            <input type="number" min={0.01} step={0.01}
+            placeholder='Enter expense amount' value={expenseAmount}
+            onChange={(e)=> { setExpenseAmount(e.target.value)}}/>
+        </div>
     </Modal>
     )
 }
